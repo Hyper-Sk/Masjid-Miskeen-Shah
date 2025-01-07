bfc5440171c859b8d714733235fcd4b9ad8138fa
crammaze <crammaze@crammazes-Mac-mini.local>
2025-01-07 11:49:47 +0530

    sixth

--- script/timings.js
+++ script/timings.js
@@ -1,8 +1,17 @@
 function receiveData() {
-  fetch("https://api.sunrisesunset.io/json?lat=17.38405&lng=78.45636")
+  fetch("https://api.aladhan.com/v1/timings/07-01-2025?latitude=17.3850&longitude=78.4867&method=2")
     .then((response) => response.json())
     .then((data) => {
       getData(data);
+    })
+    .catch((err) => console.error(err));
+
+  fetch(
+    "https://api.aladhan.com/v1/gToH/07-01-2025?latitude=17.3850&longitude=78.4867&method=2"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      dateBar(data);
     })
     .catch((err) => console.error(err));
 }
@@ -10,28 +19,40 @@ function receiveData() {
 receiveData();
 
 function getData(d) {
-//   console.log(d);
-  date = d.results.date;
-  sunrise = d.results.sunrise;
-  sunset = d.results.sunset;
-  let sunriseElement = document.querySelector(".bottombar p span");
-//   console.log(sunriseElement);
-  sunriseElement.innerHTML = sunrise;
+  sunrise = d.data.timings.Sunrise
+  sunset = d.data.timings.Sunset
+  let sunriseElement = document.querySelector(".bottombar .bottombar-content");
+  //   console.log(sunriseElement);
+  sunriseElement.innerHTML = `<p>Sunrise : ${sunrise} </p> <p>Sunset : ${sunset}</p>`;
 }
 
-function dateBar() {
-    // English date india
-    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-    let year = new Date().getFullYear()
-    let month = new Date().getMonth().toLocaleString("en-US", "Asia/Delhi");
-    let date = new Date().getDate().toLocaleString("en-US", "Asia/Delhi");
-    let today = `${date} ${months[month]} ${year}`
-    // console.log(today)
-    let dateElement = document.querySelector('.timings .date-content')
-    dateElement.innerHTML = `<p> ${today}</p>`
-    
-    
-    // hijri dates india 
-    
+function dateBar(hijriDate) {
+  // English date india
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let year = new Date().getFullYear();
+  let month = new Date().getMonth().toLocaleString("en-US", "Asia/Delhi");
+  let date = new Date().getDate().toLocaleString("en-US", "Asia/Delhi");
+  let today = `${date} ${months[month]} ${year}`;
+  // console.log(today)
+  let dateElement = document.querySelector(".timings .date-content");
+
+  // hijri dates india
+  let arabic = hijriDate.data.hijri.month.ar;
+  let english = hijriDate.data.hijri.month.en;
+  let hijriD = hijriDate.data.hijri.date;
+  dateElement.innerHTML = `<p> ${today} |  ${hijriD} ${arabic} ${english}</p>`;
+  // console.log(hi)
 }
-dateBar();
